d0fcfe2a6200768760d3f5cdf26245bd8449f630
감성은준 <129740695+Sentimentaleunjun@users.noreply.github.com>
2026-07-27 23:58:47 +0900

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,39 +6,69 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Landing from "./pages/Landing";
 import Player from "./pages/Player";
+import Setup from "./pages/Setup";
+
 
 function Router() {
-  // make sure to consider if you need authentication for certain routes
   return (
     <Switch>
-      <Route path="/" component={Landing} />
-      <Route path={"/app"} component={Player} />
-      <Route path={"/404"} component={NotFound} />
-      {/* Final fallback route */}
+
+      <Route 
+        path="/" 
+        component={Landing} 
+      />
+
+
+      <Route
+        path="/setup"
+        component={Setup}
+      />
+
+
+      <Route
+        path="/app"
+        component={Player}
+      />
+
+
+      <Route
+        path="/404"
+        component={NotFound}
+      />
+
+
       <Route component={NotFound} />
+
     </Switch>
   );
 }
 
-// NOTE: About Theme
-// - First choose a default theme according to your design style (dark or light bg), than change color palette in index.css
-//   to keep consistent foreground/background color across components
-// - If you want to make theme switchable, pass `switchable` ThemeProvider and use `useTheme` hook
 
 function App() {
+
   return (
+
     <ErrorBoundary>
+
       <ThemeProvider
         defaultTheme="light"
-        // switchable
       >
+
         <TooltipProvider>
+
           <Toaster />
+
           <Router />
+
         </TooltipProvider>
+
       </ThemeProvider>
+
     </ErrorBoundary>
+
   );
+
 }
+
 
 export default App;
